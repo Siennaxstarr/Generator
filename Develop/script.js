b0297580c10063a65b0ccef586210e56e9d230e6
Siennaxstarr <siennastarrv@gmail.com>
2023-10-05 20:51:51 -0800

    added to new repo with prior code to new repo

---
+++ Develop/script.js
@@ -0,0 +1,9 @@
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+Number = "1234567890"
+
+ans = lower_case + upper_case + Number
+
+length = 8
+password = ("travelthewrld")
+print = ("Generated password is", password)

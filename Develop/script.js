4f10f38a6070f6173e36ec7400de7bb2afbb910f
Siennaxstarr <siennastarrv@gmail.com>
2023-10-09 17:52:29 -0800

    Console Logged my Buttom Message

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,32 @@
+
 lower_case = "abcdefghijklmnopqrstuvwxyz"
 upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 Number = "1234567890"
-
 ans = lower_case + upper_case + Number
-
 length = 8
 password = ("travelthewrld")
-print = ("Generated password is", password)
+print = ("Generated password is ", password)
+
+var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+    console.log("Hello! You clicked the button!")
+// 1. Prompt the user for the password criteria
+//      a. Password Length 8 < 128
+//      b. Lowercase, uppercase, numbers, special characters
+// 2. Validate the input.
+// 3. Generate Password based on Criteria 
+
+// 4.Display password on to the page 
+ return "1xTvuMbrE2";
+}
+function writePassword(){
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+}
+
+
+generateBtn.addEventListener("click", writePassword);
+
+
+
